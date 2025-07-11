1825131b66d78c97188c748b2ffb9a57f3726a95
SamaraDcruz <samaragracedcruz@gmail.com>
2025-07-11 09:47:10 -0400

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+num = input("Enter a 4-digit number: ")
+
+# Get middle two digits
+d1 = int(num[1])
+d2 = int(num[2])
+
+# Multiply them
+product = d1 * d2
+
+print("Product of middle digits:", product)
